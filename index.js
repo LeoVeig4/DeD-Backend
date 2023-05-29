e28afede075073d09a754896b46cd1f649dcd87c
LeoVeig4 <leoveiga2002@gmail.com>
2023-05-29 11:46:46 -0300

    nothing to say

--- index.js
+++ index.js
@@ -35,9 +35,8 @@ app.get('/', (req, res) => {
 //
 //entregar uma porta
 app.listen(process.env.PORT);
-// mongoose.connect(`${process.env.CONNECT_MONGO_URL}`)
+mongoose.connect(`${process.env.CONNECT_MONGO_URL}`)
 
-//     .then(() => {
-//         console.log('Conectado ao mongoDB')
-
-//     }).catch((err) => console.log(err))
+    .then(() => {
+        console.log('Conectado ao mongoDB')
+    }).catch((err) => console.log(err))
